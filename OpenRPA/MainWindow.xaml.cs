7feb0d2e751cf26c144704eba9ad9941a72f212e
Andrea Castelli <andrea.castelli@my-invenio.com>
2021-01-21 11:08:45 +0100

    openrpa url restoration

--- OpenRPA/MainWindow.xaml.cs
+++ OpenRPA/MainWindow.xaml.cs
@@ -2649,7 +2649,7 @@ namespace OpenRPA
         }
         private void OnOpenEdgePageCommand(object _item)
         {
-            System.Diagnostics.Process.Start("msedge.exe", "https://chrome.google.com/webstore/detail/openrpa/gmpnnjghibofefmnppkgkadiijlblhia");
+            System.Diagnostics.Process.Start("msedge.exe", "https://chrome.google.com/webstore/detail/openrpa/hpnihnhlcnfejboocnckgchjdofeaphe");
         }
         
         private int lastsapprocessid = -1;
